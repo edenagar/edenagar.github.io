fba054fef836fec31eee3eec9f12d21eb9d1b880
edenagar <nagar.eden6@gmail.com>
2023-07-21 15:04:48 +0300

    first init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>EDEN NAGAR</h1>
+      </header>
+      <main>
+        <section>
+          <h2>About Me</h2>
+        </section>
+        <section>
+          <h2>Projects</h2>
+        </section>
+        <section>
+          <h2>Contact</h2>
+        </section>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
